8847a39dccbf70e14b7d8479a90259cc77789891
xyingsoft <xyingsoft@gmail.com>
2023-07-24 11:05:34 -0400

    Force close chrome

--- as_helper.rb
+++ as_helper.rb
@@ -3,7 +3,8 @@ module AsHelper
 
   def close_chrome
     script = <<~APPLESCRIPT
-      do shell script {"pkill Google\ Chrome"}
+      -- do shell script {"kill -9 $(pidof Google\ Chrome)"}
+      do shell script {"killall -9 Google\ Chrome"}
     APPLESCRIPT
 
     `osascript -e '#{script}'`
